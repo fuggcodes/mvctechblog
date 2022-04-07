3c6c4cb265b929600ad381860e66c3bafc78e834
David Binyatov <davidbphotography101@gmail.com>
2022-04-07 03:46:23 -0400

    establish const for sess

--- server.js
+++ server.js
@@ -8,3 +8,13 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/config");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+    secret: "Super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
