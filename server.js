8d12f0d2981e9769afda0b2377a0db9107443ca3
David Binyatov <davidbphotography101@gmail.com>
2022-04-07 03:50:22 -0400

    server.js

--- server.js
+++ server.js
@@ -28,3 +28,17 @@ const sess = {
       },
     },
   });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./controllers/"));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
+});
