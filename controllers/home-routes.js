16facad793844faeed4227fd9166352d53ab3766
David Binyatov <davidbphotography101@gmail.com>
2022-04-07 04:04:29 -0400

    get single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,3 +15,28 @@ router.get("/", (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  // get single post
+router.get("/post/:id", (req, res) => {
+    Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    })
+      .then((dbPostData) => {
+        if (dbPostData) {
+          const post = dbPostData.get({ plain: true });
+  
+          res.render("single-post", { post });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
